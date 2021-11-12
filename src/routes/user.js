e4faf3941315f1fb2b01441da04546e084a25ba5
lucas-andrad <lafdesouza2002@outlook.com>
2021-11-12 03:23:21 -0300

    Feat: Add new userRoutes

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,10 +2,13 @@ const { UserController } = require('../controllers');
 
 const app = require('express')();
 
-app.get('/', async(req, res) => new UserController().getUsers(req, res));
-app.get('/:id', async (req, res) => new UserController().getUser(req, res));
+app.get('/', (req, res) => new UserController().getUsers(req, res));
+app.get('/:id', (req, res) => new UserController().getUser(req, res));
 app.post('/signup', (req, res) => new UserController().signUp(req, res));
 app.post('/login', (req, res) => new UserController().login(req, res));
+app.put('/:id', (req, res) => new UserController().getUser(req, res));
+app.delete('/:id', (req, res) => new UserController().deleteUser(req, res));
+
 
 
 module.exports = app;
