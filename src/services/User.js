befdd522ef0bb1a68e0dc824ce39a12bbda12267
root <root@LAPTOP-LPDG5RQE.localdomain>
2021-11-12 03:08:29 -0300

    Feat: Update package.json

--- src/services/User.js
+++ src/services/User.js
@@ -1,9 +1,11 @@
 const dependencies = {
-    makeUserObject: require("../utils/makeUserObject"),
+    userFactory: require("../utils/factoryUser"),
     userValidator: require("../utils/userValidator"),
     Model: require('../models').User,
     Crypto: require("../utils/Crypto"),
     Jwt: require("../jwt"),
+    isEqual: require('lodash').isEqual,
+    mongoose: require('mongoose'),
 }
 
 class User {
@@ -19,11 +21,11 @@ class User {
                 Jwt
             } = this._dependencies;
             const { email } = data;
-            const { _id, password } = await new Model().findByEmail(email);
-            const encryptedPassword = new Crypto().encrypt(data.password);
-            if (encryptedPassword === password) {
-                const token = await new Jwt().sign(_id, email);
-                return { token };
+            const { _id, name, password } = await new Model().findByEmail(email);
+            const { data } = new Crypto().encrypt(data.password);
+            if (data === password) {
+                const token = await new Jwt().sign(_id, email, name);
+                return { _id, email, name, token };
             }
             throw new Error('Incorrect email or password');
         } catch (error) {
@@ -33,7 +35,7 @@ class User {
 
     async createUser(data) {
         const { 
-            makeUserObject,
+            userFactory,
             userValidator,
             Model,
             Crypto,
@@ -44,17 +46,55 @@ class User {
             throw new Error('Password was not provided');
         }
         data.password = new Crypto().encrypt(data.password);
-        const object = makeUserObject(data);
+        const model = new Model();
+        const object = userFactory(data);
         const validated = userValidator(object);
         if (validated) {
-            const userCreated = await new Model().create(validated);
+            const userCreated = await model.create(validated);
             if (!userCreated) throw new Error('Error with data creation');
-            const { _id, name } = userCreated;
-            const token = await new Jwt().sign(_id, name);
+            const { _id, email, name } = userCreated;
+            const token = await new Jwt().sign(_id, email, name);
             if (!token) throw new Error('Incorrect email or password')
-            return { token };
+            return { _id, email, name, token };
         }
     }
+
+    async editUser(data, params, authorization) {
+        const { 
+            userFactory,
+            userValidator,
+            Model,
+            Jwt,
+            isEqual
+        } = this._dependencies;
+        const validated = userValidator(object);
+        if (validated) {
+            const userFromToken = new Jwt().verify(authorization);
+            const userFromDatabase = await new Model().findById(params.id, { _id: 1 });
+            const isEqual = isEqual(String(userFromDatabase._id), String(userFromToken._id));
+            if (!isEqual) throw new Error('Usuário não tem permissão para realizar essa ação');
+            const object = userFactory(data);
+            return new Model().update(params.id, object);
+        };
+    };
+
+    async deleteUser(authorization, params) {
+        const { 
+            userFactory,
+            userValidator,
+            Model,
+            Jwt,
+            isEqual,
+        } = this._dependencies;
+        console.log(authorization)
+
+        const userFromToken = new Jwt().verify(authorization);
+        const userFromDatabase = await new Model().findById(params.id, { _id: 1 });
+
+        const isSameUser = isEqual(String(userFromDatabase._id), String(userFromToken._id));
+        if (!isSameUser) throw new Error('Usuário não tem permissão para realizar essa ação');
+        return new Model().delete(params.id);
+    };
 }
 
 module.exports = User;
