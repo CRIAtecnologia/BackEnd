37587d5c19c9d464bede070bd41a0b663b4646c4
lucas-andrad <lafdesouza2002@outlook.com>
2021-11-12 03:20:36 -0300

    Feat: Add edit and delete methods to userController

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -5,7 +5,6 @@ const dependencies = {
 
 class UserController {
     constructor(injection) {
-
         this._dependencies = Object.assign({}, injection, dependencies);
     }
 
@@ -13,9 +12,9 @@ class UserController {
         const { Service } = this._dependencies;
         try {
             const result = await new Service().createUser(req.body);        
-            return res.send(result);
+            return res.status(201).send(result);
         } catch (error) {
-            return res.status(500).send(`We found an ${error}`);
+            return res.status(500).send(`We found an error: ${error}`);
         }
 
     }
@@ -26,7 +25,7 @@ class UserController {
             const result = await new Service().login(req.body);
             return res.send(result);
         } catch (error) {
-            return res.status(500).send(`We found an ${error}`);
+            return res.status(500).send(`We found an error: ${error}`);
         }
     }
 
@@ -37,18 +36,45 @@ class UserController {
             const data = await new Model().findById(id);
             return res.status(200).send(data);
         } catch (error) {
-            return res.status(500).send(`We found an ${error}`);
+            return res.status(500).send(`We found an error: ${error}`);
+        }
+    }
+
+    async editUser(req, res) {
+        const { Service } = this._dependencies;
+        try {
+            const result = await new Service().editUser(req.body, req.params, req.headers.authorization);
+            return res.status(200).send(result)
+        } catch (error) {
+            return res.status(500).send(`We found an error: ${error}`);
+        }
+    }
+
+    async deleteUser(req, res) {
+        const { Service } = this._dependencies;
+        try {
+            console.log(req.headers)
+
+            const result = await new Service().deleteUser(req.headers.authorization, req.params);
+            if (result) return res.status(204).send({ deleted: true });
+        } catch (error) {
+            return res.status(500).send(`We found an error: ${error}`);
         }
     }
 
     async getUsers(req, res) {
         const { Model } = this._dependencies;
         try {
-            const data = await new Model().findAll();
-            return res.status(200).send(data);
+            const userModel = new Model();
+            const data = await userModel.findAll({ password: 0 }, req.body);
+            const total = await userModel.countAll();
+            return res.status(200).send({
+                data,
+                total,
+            });
         } catch (error) {
-            return res.status(500).send(`We found an ${error}`);
-        }    
+            return res.status(500).send(`We found an error: ${error}`);
+        }
     }
 }
 
