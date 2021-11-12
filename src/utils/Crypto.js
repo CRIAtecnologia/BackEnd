387ccb9d001c03b1c6643a4b6769c265ab27ae9b
lucas-andrad <lafdesouza2002@outlook.com>
2021-11-12 03:23:37 -0300

    Fix: Change crypto methods

--- src/utils/Crypto.js
+++ src/utils/Crypto.js
@@ -1,22 +1,33 @@
 const dependencies = {
     crypto: require('crypto'),
     key: process.env.KEY,
+    iv: process.env.IV,
 };
 
 class Crypto {
-    encrypt(password) {
-        const {
-            crypto,
-            key,
-        } = dependencies;
-        const hmac = crypto.createHmac('sha256', key);
-        hmac.update(password);
-        return hmac.digest('base64');
-    }
+    encrypt(text) {
+        const { key, crypto, iv } = dependencies;
+        const cipher = 
+           crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+        let encrypted = cipher.update(text);
 
-    verifier(hashedPassword) {
+        encrypted = Buffer.concat([encrypted, cipher.final()]);
 
+        return encrypted.toString('hex');
     }
+    decrypt(text) {
+        const { key, crypto, iv } = dependencies;
+        const encryptedText =
+           Buffer.from(text, 'hex');
+         
+        const decipher = crypto.createDecipheriv(
+               'aes-256-cbc', Buffer.from(key), iv);
+        
+        let decrypted = decipher.update(encryptedText);
+        decrypted = Buffer.concat([decrypted, decipher.final()]);
+         
+        return decrypted.toString();
+       }
 };
 
 module.exports = Crypto;
